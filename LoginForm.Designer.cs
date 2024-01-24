edba5e4054f872da6fa11af944d95707d3d883b1
CodeCraftKJ <krzysztofjofficial@gmail.com>
2024-01-24 17:14:59 +0100

    LoginFormUpdate

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -29,21 +29,94 @@ namespace QBankingSystem
         /// </summary>
         private void InitializeComponent()
         {
+            this.labUsername = new System.Windows.Forms.Label();
+            this.labPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.linkForgotPassword = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // Form1
+            // labUsername
             // 
+            this.labUsername.AutoSize = true;
+            this.labUsername.Location = new System.Drawing.Point(349, 108);
+            this.labUsername.Name = "labUsername";
+            this.labUsername.Size = new System.Drawing.Size(75, 20);
+            this.labUsername.TabIndex = 0;
+            this.labUsername.Text = "Username";
+            // 
+            // labPassword
+            // 
+            this.labPassword.AutoSize = true;
+            this.labPassword.Location = new System.Drawing.Point(349, 181);
+            this.labPassword.Name = "labPassword";
+            this.labPassword.Size = new System.Drawing.Size(70, 20);
+            this.labPassword.TabIndex = 1;
+            this.labPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(238, 131);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(301, 27);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(238, 204);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(301, 27);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(238, 295);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(301, 29);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Log in";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // linkForgotPassword
+            // 
+            this.linkForgotPassword.AutoSize = true;
+            this.linkForgotPassword.Location = new System.Drawing.Point(324, 327);
+            this.linkForgotPassword.Name = "linkForgotPassword";
+            this.linkForgotPassword.Size = new System.Drawing.Size(125, 20);
+            this.linkForgotPassword.TabIndex = 5;
+            this.linkForgotPassword.TabStop = true;
+            this.linkForgotPassword.Text = "Forgot Password?";
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.btnLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.Add(this.linkForgotPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.labPassword);
+            this.Controls.Add(this.labUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labUsername;
+        private System.Windows.Forms.Label labPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.LinkLabel linkForgotPassword;
     }
 }
 
--- LoginForm.cs
+++ LoginForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace QBankingSystem
@@ -17,9 +10,53 @@ namespace QBankingSystem
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnLogin_Click_1(object sender, EventArgs e)
         {
+            // Here, you'd typically validate the credentials against a database or other storage
+            // For now, let's just check if the fields are not empty
 
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                // Display an error message if either field is empty
+                MessageBox.Show("Please enter both username and password.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // TODO: Validate credentials against a database or user repository
+                bool isValidUser = ValidateUser(username, password);
+
+                if (isValidUser)
+                {
+                    // If the credentials are valid, show a success message and open the main dashboard
+                    MessageBox.Show("Login successful!", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // TODO: Open the main dashboard form and close the login form
+                    AccountForm mainDashboard = new AccountForm();
+                    mainDashboard.Show();
+                    this.Hide(); // Hide the login form
+                }
+                else
+                {
+                    // If the credentials are not valid, show an error message
+                    MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool ValidateUser(string username, string password)
+        {
+            // TODO: Implement actual user validation logic here (e.g., check against a database)
+            // For now, let's assume the credentials are valid
+            return true;
+        }
+
+        private void linkForgotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // TODO: Open the form to reset the password or create a new account
+            MessageBox.Show("Link to password reset or registration not implemented yet.", "Not Implemented", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
